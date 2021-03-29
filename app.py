7482101126ff3eaae5ac9b7220212ab76be23cef
BrianDouglas <bddrew86@gmail.com>
2021-03-29 16:23:38 -0700

    added cli function to update a json file by querying the db full db and aggregating the records into state: action counts

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import time
 from datetime import datetime
 import json
 import pymongo
@@ -39,6 +40,33 @@ def game_data():
         print(data)
         print(type(data))
     return "OK", 200
+
+@app.cli.command()
+def queryDB():
+    '''Run a full query of the DB and update a local json file'''
+    os.environ["PASSWORD"] = 'bd2411008'
+    os.environ["USERNAME"] = 'admin'
+    # establish connection
+    connectTo = 'final_project'
+    client = pymongo.MongoClient(f"mongodb+srv://{os.getenv('USERNAME')}:{os.getenv('PASSWORD')}@bricluster.yskth.mongodb.net/{connectTo}?retryWrites=true&w=majority")
+    db = client.final_project
+    collection = db.state_action
+    #query all records
+    result = collection.find({})
+    # loop over all records and create a json file with a key for each unique state and a count of actions for each state
+    json_dumper = {}
+    for doc in result:
+        for pair in doc['gameplay']:
+            cur_action = pair['action']
+            cur_state = str(pair['state'])
+            if cur_state in json_dumper:
+                json_dumper[cur_state][cur_action] += 1
+            else:
+                json_dumper[cur_state] = {"RIGHT":0,"LEFT":0,"UP":0,"DOWN":0}
+                json_dumper[cur_state][cur_action] += 1
+
+    with open("static/agg_state_actions.json", 'w') as out_file:
+        json.dump(json_dumper, out_file, indent=2)
 
 if __name__ == "__main__":
     app.run(debug=True)
